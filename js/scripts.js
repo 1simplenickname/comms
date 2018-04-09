e2281124584c55518689aa9375f03b2b1e76d5e5
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-04-09 21:39:24 +0300

    Revamped the loginScreen by making it small and adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 function login() {
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("mailing").style.visibility = "hidden";
+    document.getElementById("question").style.visibility = "hidden";
     document.getElementById("loginScreen").style.visibility = "visible";
 }
 
-function loginScreenCross() {
+// function loginScreenCross() {
+//     document.getElementById("loginScreen").style.visibility = "hidden";
+//     document.getElementById("logo").style.visibility = "visible";
+// }
+
+function loginScreenSubmit() {
     document.getElementById("loginScreen").style.visibility = "hidden";
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("mailing").style.visibility = "visible";
+    document.getElementById("question").style.visibility = "visible";
 }
 
 function mailing() {
