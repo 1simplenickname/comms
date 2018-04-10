7a1f226bc04ff5a1efa9b5bd923ce4bb289a0db7
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-04-10 20:48:42 +0300

    Started working on the mailingScreen

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,17 @@ function loginScreenSubmit() {
 }
 
 function mailing() {
-    window.open("#","_self")
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("mailing").style.visibility = "hidden";
+    document.getElementById("question").style.visibility = "hidden";
+    document.getElementById("mailingScreen").style.visibility = "visible";
+}
+
+function mailingScreenCross() {
+    document.getElementById("mailingScreen").style.visibility = "hidden";
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("mailing").style.visibility = "visible";
+    document.getElementById("question").style.visibility = "visible";
 }
 
 function question() {
