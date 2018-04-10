a046acf53c313bb6dfaf8da616bf1d6ed02532e8
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-04-10 21:48:25 +0300

    Renamed mailingScreen and all of its components to signupScreen, and almost finished it

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function login() {
     document.getElementById("login").style.visibility = "hidden";
-    document.getElementById("mailing").style.visibility = "hidden";
+    document.getElementById("signup").style.visibility = "hidden";
     document.getElementById("question").style.visibility = "hidden";
     document.getElementById("loginScreen").style.visibility = "visible";
 }
@@ -8,24 +8,38 @@ function login() {
 function loginScreenSubmit() {
     document.getElementById("loginScreen").style.visibility = "hidden";
     document.getElementById("login").style.visibility = "visible";
-    document.getElementById("mailing").style.visibility = "visible";
+    document.getElementById("signup").style.visibility = "visible";
     document.getElementById("question").style.visibility = "visible";
 }
 
-function mailing() {
+function signup() {
     document.getElementById("login").style.visibility = "hidden";
-    document.getElementById("mailing").style.visibility = "hidden";
+    document.getElementById("signup").style.visibility = "hidden";
     document.getElementById("question").style.visibility = "hidden";
-    document.getElementById("mailingScreen").style.visibility = "visible";
+    document.getElementById("signupScreen").style.visibility = "visible";
 }
 
-function mailingScreenCross() {
-    document.getElementById("mailingScreen").style.visibility = "hidden";
+function passwordMatch() {
+    let password = $("#signupScreenPasswordField").value;
+    let passwordRepeat = $("#signupScreenPasswordRepeatField").value;
+
+    if (password != passwordRepeat)
+        document.getElementById("signupScreenPasswordField").style.color = "red";
+    else
+        document.getElementById("signupScreenPasswordField").style.color = "green";
+}
+
+function signupScreenClose() {
+    document.getElementById("signupScreen").style.visibility = "hidden";
     document.getElementById("login").style.visibility = "visible";
-    document.getElementById("mailing").style.visibility = "visible";
+    document.getElementById("signup").style.visibility = "visible";
     document.getElementById("question").style.visibility = "visible";
 }
 
 function question() {
     window.open("#","_self")
 }
+
+$(document).ready(function () {
+    $("#signupScreenPasswordField, #signupScreenPasswordRepeatField").keyup(passwordMatch);
+});
