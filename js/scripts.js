4b152a2b200819f66bf81128d9f53f4e1b5310ef
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-05-04 18:34:42 +0300

    Made passwordSecurity() less redundant

--- js/scripts.js
+++ js/scripts.js
@@ -31,37 +31,35 @@ function question() {
 }
 
 function passwordStrength() {
-    let password = document.getElementById("signupScreenPasswordField").value;
-    let passwordField = document.getElementById("signupScreenPasswordField");
-    let passwordRepeatField = document.getElementById("signupScreenPasswordRepeatField");
+    let password = document.getElementById("signupScreenPasswordField");
     let strength = 0;
 
-    if  (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+    if  (password.value.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
         strength += 1
     }
-    if (password.match(/[!?@£$%^&*()~<>]+/)) {
+    if (password.value.match(/[!?@£$%^&*()~<>]+/)) {
         strength += 1
     }
-    if (password.length > 5) {
+    if (password.value.length > 5) {
         strength += 1
     }
 
     switch(strength) {
         // very weak password
         case 0:
-            passwordField.style.color = "#EF2E2E";
+            password.style.color = "#EF2E2E";
             break;
         // weak password
         case 1:
-            passwordField.style.color = "#EF7E2E";
+            password.style.color = "#EF7E2E";
             break;
         // strong password
         case 2:
-            passwordField.style.color = "#DFAF2B";
+            password.style.color = "#DFAF2B";
             break;
         // very strong password
         case 3:
-            passwordField.style.color = "#24B936";
+            password.style.color = "#24B936";
             break;
     }
 }
