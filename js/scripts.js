df84d4fc71e16f2f8ec8fd0411c5d88b09d56edb
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-05-04 18:32:14 +0300

    Finished the password security checker

--- js/scripts.js
+++ js/scripts.js
@@ -30,42 +30,38 @@ function question() {
     window.open("#","_self")
 }
 
-// function passwordStrength() {
-//     let password = String(document.getElementById("signupScreenPasswordField"));
-//     let passwordField = document.getElementById("signupScreenPasswordField");
-//     let passwordRepeatField = document.getElementById("signupScreenPasswordRepeatField");
-//     let strength = 0;
-//
-//     if  (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
-//         strength += 1;
-//     }
-//     if (password.match(/[!?@£$%^&*()~<>]+/)) {
-//         strength += 1;
-//     }
-//     if (password.length > 6) {
-//         strength += 1;
-//     }
-//
-//     switch(strength) {
-//         // very weak password
-//         case 0:
-//             passwordField.style.color = "red";
-//             passwordRepeatField.style.color = "red";
-//             break;
-//         // weak password
-//         case 1:
-//             passwordField.style.color = "orange";
-//             passwordRepeatField.style.color = "orange";
-//             break;
-//         // strong password
-//         case 2:
-//             passwordField.style.color = "yellow";
-//             passwordRepeatField.style.color = "yellow";
-//             break;
-//         // very strong password
-//         case 3:
-//             passwordField.style.color = "green";
-//             passwordRepeatField.style.color = "green";
-//             break;
-//     }
-// }
+function passwordStrength() {
+    let password = document.getElementById("signupScreenPasswordField").value;
+    let passwordField = document.getElementById("signupScreenPasswordField");
+    let passwordRepeatField = document.getElementById("signupScreenPasswordRepeatField");
+    let strength = 0;
+
+    if  (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)) {
+        strength += 1
+    }
+    if (password.match(/[!?@£$%^&*()~<>]+/)) {
+        strength += 1
+    }
+    if (password.length > 5) {
+        strength += 1
+    }
+
+    switch(strength) {
+        // very weak password
+        case 0:
+            passwordField.style.color = "#EF2E2E";
+            break;
+        // weak password
+        case 1:
+            passwordField.style.color = "#EF7E2E";
+            break;
+        // strong password
+        case 2:
+            passwordField.style.color = "#DFAF2B";
+            break;
+        // very strong password
+        case 3:
+            passwordField.style.color = "#24B936";
+            break;
+    }
+}
